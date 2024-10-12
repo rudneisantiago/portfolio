f0627a9402f3dd923c752c5004a9980c1f0c59df
Rudnei Santiago <rudnei.santiago@gmail.com>
2024-10-12 20:26:45 -0300

    Criação de h1 padrão e alteração do hero

---
+++ components/h1.tsx
@@ -0,0 +1,9 @@
+function H1(props) {
+  return (
+    <h1 className="text-4xl font-bold my-8 text-dark-blue-950">
+      {props.children}
+    </h1>
+  );
+}
+
+export default H1;
--- components/hero.tsx
+++ components/hero.tsx
@@ -1,14 +1,19 @@
+import H1 from "./h1";
+
 function Hero() {
   return (
     <div className="w-full h-fit bg-gradient-to-l from-dark-blue-600 to-dark-blue-200">
       <div className="container mx-auto">
-        <div className="flex flex-wrap gap-8 px-6 py-12 justify-center items-center sm:justify-between ">
-          <p className="w-full sm:w-1/2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet
-            consectetur deserunt id ab sit ducimus porro eveniet, molestiae
-            rerum explicabo architecto libero optio, nisi harum tempore minus
-            fugiat cum animi?
-          </p>
+        <div className="flex flex-wrap gap-8 px-6 py-12 justify-center items-center sm:justify-around ">
+          <div className="w-full sm:w-1/2">
+            <H1>Oi, eu sou o Rudnei</H1>
+            <p>
+              Criei essa página para falar um pouco mais sobre mim, minha
+              tragetória profissional, meus aprendizados, habilidades técnicas e
+              curiosidades. Além, é claro, de falar um pouco de mim como pessoa,
+              hobbies, gostos e afins. Fique a vontade ;)
+            </p>
+          </div>
           <img
             className="rounded-full"
             src="https://avatars.githubusercontent.com/rudneisantiago"
